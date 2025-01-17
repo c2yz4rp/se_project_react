eccb699b59cfe7744da96cda0ccdc3c6ec4aec21
Cinthia Patino <cinyatzy19@gmail.com>
2025-01-16 23:51:51 -0600

    fetch api

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,16 +4,16 @@ function checkResponse(res) {
   return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
 }
 
-function request(url, options) {
-  return fetch(url, options).then(checkResponse);
-}
+//function request(url, options) {
+// return fetch(url, options).then(checkResponse);
+//}
 
 function getItems() {
-  return request(`${baseUrl}/items`);
+  return fetch(`${baseUrl}/items`).then(checkResponse);
 }
 
 function addItem(item) {
-  return request(`${baseUrl}/items`, {
+  return fetch(`${baseUrl}/items`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
@@ -21,14 +21,14 @@ function addItem(item) {
       imageUrl: item.imageUrl,
       weather: item.weather,
     }),
-  });
+  }).then(checkResponse);
 }
 
 function deleteItem(id) {
-  return request(`${baseUrl}/items/${id}`, {
+  return fetch(`${baseUrl}/items/${id}`, {
     method: "DELETE",
     headers: { "Content-Type": "application/json" },
-  });
+  }).then(checkResponse);
 }
 
 export { getItems, addItem, deleteItem };
